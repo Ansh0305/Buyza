a2a3166ffdb6d3af9b10e5474dee13cc45b82432
Ansh0305 <saiansh2016@gmail.com>
2026-01-19 23:59:07 +0530

    Footer styling and responsiveness done!

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
+    <div className='mt-16 flex flex-col items-cente gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg'>
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo.png"
+            alt="Buyza"
+            width={36}
+            height={36}
+          />
+          <p className="hidden md:block text-md font-medium tracking-wider text-white">Buyza.</p>
+        </Link>
+          <p className="text-sm text-gray-100"> &#169; Buyza</p>
+          <p className="text-sm text-gray-100"> All rights reserved.</p>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contacts</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">All Products</Link>
+        <Link href="/">New Arrivals</Link>
+        <Link href="/">Best Sellers</Link>
+        <Link href="/">Sale</Link>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">Affiliate Program</Link>
+      </div>
+
+    </div>
   )
 }
 
