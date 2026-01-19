abb52fb3dd79f496198fbb3dfe24cef4b02bbcfe
Ansh0305 <saiansh2016@gmail.com>
2026-01-20 00:11:05 +0530

    Added Featured product image

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image"
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured product" fill/>
+      </div>
+    </div>
   )
 }
 
