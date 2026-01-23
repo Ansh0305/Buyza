0b09a27445f94b060b56bbcc461a3c05bd395610
Ansh0305 <saiansh2016@gmail.com>
2026-01-24 00:12:15 +0530

    Product interaction (Size) and styling is done

--- client/src/components/ProductInteraction.tsx
+++ client/src/components/ProductInteraction.tsx
@@ -1,9 +1,63 @@
-"use client"
+"use client";
 
-const ProductInteraction = () => {
+import { ProductType } from "@/types";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const ProductInteraction = ({
+  product,
+  selectedSize,
+  selectedColor,
+}: {
+  product: ProductType;
+  selectedSize: string;
+  selectedColor: string;
+}) => {
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleTypeChange = (type:string, value:string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(type, value)
+      router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  }
   return (
-    <div>ProductInteraction</div>
-  )
-}
+    <div className="flex flex-col gap-4 mt-4">
+      {/* Size */}
+      <div className="flex flex-col gap-2 text-xs">
+        <span className="text-gray-500">Size</span>
 
-export default ProductInteraction
+        <div className="flex items-center gap-2">
+          {product.sizes.map((size) => (
+            <div
+              className={`cursor-pointer border-1 p-[2px] ${
+                selectedSize === size ? "border-gray-600" : "border-gray-300"
+              }`}
+              key={size}
+              onClick={() => handleTypeChange("size", size)}
+            >
+              <div
+                className={`w-6 h-6 text-center flex items-center justify-center ${
+                  selectedSize === size
+                    ? "bg-black text-white"
+                    : "bg-white text-black"
+                }`}
+              >
+                {size.toUpperCase()}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Color */}
+      <div className="flex flex-col gap-2 text-sm"></div>
+
+      {/* Quantity */}
+      <div className="flex flex-col gap-2 text-sm"></div>
+    </div>
+  );
+};
+
+export default ProductInteraction;
