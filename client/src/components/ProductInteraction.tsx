53984d43147af0b8871e35c5b177ede714844021
Ansh0305 <saiansh2016@gmail.com>
2026-01-24 00:34:07 +0530

    Add to cart and Buy the item buttons are created

--- client/src/components/ProductInteraction.tsx
+++ client/src/components/ProductInteraction.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProductType } from "@/types";
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, ShoppingCart } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
@@ -14,25 +14,24 @@ const ProductInteraction = ({
   selectedSize: string;
   selectedColor: string;
 }) => {
-
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
-  const handleTypeChange = (type:string, value:string) => {
-      const params = new URLSearchParams(searchParams.toString())
-      params.set(type, value)
-      router.push(`${pathname}?${params.toString()}`, { scroll: false });
-  }
+  const handleTypeChange = (type: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(type, value);
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
 
-  const handleQuantityChange =(type: "increment" | "decrement") => {
+  const handleQuantityChange = (type: "increment" | "decrement") => {
     if (type === "increment") {
-      setQuantity((prev) => prev + 1)
+      setQuantity((prev) => prev + 1);
     } else if (quantity > 1) {
-        setQuantity((prev) => prev - 1)
+      setQuantity((prev) => prev - 1);
     }
-  }
+  };
   return (
     <div className="flex flex-col gap-4 mt-4">
       {/* Size */}
@@ -102,6 +101,17 @@ const ProductInteraction = ({
           </button>
         </div>
       </div>
+
+      {/* Buttons*/}
+      <button className="bg-gray-800 text-white px-4 py-4 rounded-md shadow-lg flex items-center justify-center gap-2 cursor-pointer text-sm font-medium">
+        <Plus />
+        Add to Cart
+      </button>
+
+      <button className="ring-1 ring-gray-400 shadow-lg text-gray-800 px-4 py-2 rounded-md flex items-center justify-center gap-2 cursor-pointer text-sm font-medium">
+        <ShoppingCart />
+        Buy this Item
+      </button>
     </div>
   );
 };
