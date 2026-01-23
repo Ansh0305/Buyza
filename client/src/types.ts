6289b9dfae27c5dba8fe7ac7ee38a1ff0f98d130
Ansh0305 <saiansh2016@gmail.com>
2026-01-23 14:20:12 +0530

    Implement and aded payment form validation schema using Zod

--- client/src/types.ts
+++ client/src/types.ts
@@ -10,7 +10,7 @@ export type ProductType = {
   colors: string[];
   images: Record<string, string>;
 };
- 
+
 export type ProductsType = ProductType[];
 
 export type CartItemType = ProductType & {
@@ -26,7 +26,7 @@ export const shippingFormSchema = z.object({
   email: z.email().min(1, "Email is required!"),
   phone: z
     .string()
-    .transform((val) => val.replace(/\D/g, "")) 
+    .transform((val) => val.replace(/\D/g, ""))
     // remove spaces, +, -
     .refine((val) => val.length === 10, {
       message: "Phone number must be exactly 10 digits!",
@@ -36,4 +36,18 @@ export const shippingFormSchema = z.object({
   state: z.string().min(1, "State is required!"),
 });
 
-export type ShippingFormInputs = z.infer<typeof shippingFormSchema>
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required!"),
+  cardNumber: z
+    .string()
+    .min(16, "Card number is required!")
+    .max(16, "Card number is required!"),
+  expiry: z
+    .string()
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expity date must be in MM/YY format!"),
+  cvv: z.string().min(3, "CVV is required!").max(3, "CVV is required!"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
