98b81f60081de3b109dd166b2b7d5a080f90acb4
Ansh0305 <saiansh2016@gmail.com>
2026-01-23 19:24:30 +0530

    Added remove to cart logic

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -7,6 +7,7 @@ import { ArrowRight, Trash2 } from "lucide-react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import Image from "next/image";
+import useCartStore from "../stores/cartStore";
 
 const steps = [
   {
@@ -24,69 +25,71 @@ const steps = [
   },
 ];
 
-const cartItems: CartItemsType = [
-  {
-    id: 1,
-    name: "Adidas CoreFit T-Shirt",
-    shortDescription:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    description:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    price: 39.9,
-    sizes: ["s", "m", "l", "xl", "xxl"],
-    colors: ["gray", "purple", "green"],
-    images: {
-      gray: "/products/1g.png",
-      purple: "/products/1p.png",
-      green: "/products/1gr.png",
-    },
-    quantity: 1,
-    selectedSize: "m",
-    selectedColor: "purple",
-  },
-  {
-    id: 2,
-    name: "Puma Ultra Warm Zip",
-    shortDescription:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    description:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    price: 59.9,
-    sizes: ["s", "m", "l", "xl"],
-    colors: ["gray", "green"],
-    images: { gray: "/products/2g.png", green: "/products/2gr.png" },
-    quantity: 1,
-    selectedSize: "m",
-    selectedColor: "gray",
-  },
-  {
-    id: 3,
-    name: "Nike Air Essentials Pullover",
-    shortDescription:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    description:
-      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
-    price: 69.9,
-    sizes: ["s", "m", "l"],
-    colors: ["green", "blue", "black"],
-    images: {
-      green: "/products/3gr.png",
-      blue: "/products/3b.png",
-      black: "/products/3bl.png",
-    },
-    quantity: 1,
-    selectedSize: "m",
-    selectedColor: "black",
-  },
-];
+// // Temporary
+// const cartItems: CartItemsType = [
+//   {
+//     id: 1,
+//     name: "Adidas CoreFit T-Shirt",
+//     shortDescription:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     description:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     price: 39.9,
+//     sizes: ["s", "m", "l", "xl", "xxl"],
+//     colors: ["gray", "purple", "green"],
+//     images: {
+//       gray: "/products/1g.png",
+//       purple: "/products/1p.png",
+//       green: "/products/1gr.png",
+//     },
+//     quantity: 1,
+//     selectedSize: "m",
+//     selectedColor: "purple",
+//   },
+//   {
+//     id: 2,
+//     name: "Puma Ultra Warm Zip",
+//     shortDescription:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     description:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     price: 59.9,
+//     sizes: ["s", "m", "l", "xl"],
+//     colors: ["gray", "green"],
+//     images: { gray: "/products/2g.png", green: "/products/2gr.png" },
+//     quantity: 1,
+//     selectedSize: "m",
+//     selectedColor: "gray",
+//   },
+//   {
+//     id: 3,
+//     name: "Nike Air Essentials Pullover",
+//     shortDescription:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     description:
+//       "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+//     price: 69.9,
+//     sizes: ["s", "m", "l"],
+//     colors: ["green", "blue", "black"],
+//     images: {
+//       green: "/products/3gr.png",
+//       blue: "/products/3b.png",
+//       black: "/products/3bl.png",
+//     },
+//     quantity: 1,
+//     selectedSize: "m",
+//     selectedColor: "black",
+//   },
+// ];
 
 const CartPage = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
-
   const [shippingForm, setShippingForm] = useState<ShippingFormInputs>();
 
   const activeStep = parseInt(searchParams.get("step") || "1");
+
+  const {cart, removeFromCart} = useCartStore();
 
   return (
     <div className="flex flex-col gap-8 items-center justify-center mt-12">
@@ -119,9 +122,9 @@ const CartPage = () => {
 
         <div className=" w-full lg:w-7/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
           {activeStep === 1 ? (
-            cartItems.map((item) => (
+            cart.map((item) => (
               // Single Cart Item
-              <div className="flex items-center justify-between" key={item.id}>
+              <div className="flex items-center justify-between" key={item.id + item.selectedSize + item.selectedColor}>
                 {/* Image and details */}
                 <div className="flex gap-8">
                   {/* Image */}
@@ -156,13 +159,13 @@ const CartPage = () => {
                 </div>
 
                 {/* Delete button  */}
-                <button className="w-8 h-8 rounded-full bg-red-100 hover:bg-red-200 transition-all duration-200 text-red-400 flex items-center justify-center cursor-pointer">
+                <button onClick={()=>removeFromCart(item)} className="w-8 h-8 rounded-full bg-red-100 hover:bg-red-200 transition-all duration-200 text-red-400 flex items-center justify-center cursor-pointer">
                   <Trash2 className="w-3 h-3" />
                 </button>
               </div>
             ))
           ) : activeStep === 2 ? (
-            <ShippingForm setShippingForm = {setShippingForm} />
+            <ShippingForm setShippingForm={setShippingForm} />
           ) : activeStep === 3 && shippingForm ? (
             <PaymentForm />
           ) : (
@@ -182,7 +185,7 @@ const CartPage = () => {
 
               <p className="font-medium">
                 $
-                {cartItems
+                {cart
                   .reduce((acc, item) => acc + item.price * item.quantity, 0)
                   .toFixed(2)}
               </p>
@@ -207,7 +210,7 @@ const CartPage = () => {
 
               <p className="font-medium">
                 $
-                {cartItems
+                {cart
                   .reduce((acc, item) => acc + item.price * item.quantity, 0)
                   .toFixed(2)}
               </p>
--- client/src/app/stores/cartStore.ts
+++ client/src/app/stores/cartStore.ts
@@ -3,22 +3,46 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
-  persist((set) => ({
-    cart: [],
-    addToCart: (product) =>
-      set((state) => ({ cart: [...state.cart, product] })),
-    removeFromCart: (product) =>
-      set((state) => ({
-        cart: state.cart.filter((p) => p.id !== product.id),
-        product,
-      })),
-    clearCart: () => set({ cart: [] }),
-  }),
-  {
-    name:"cart",
-    storage: createJSONStorage(()=>localStorage)
-  }
-)
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (product) =>
+        set((state) => {
+          const existingIndex = state.cart.findIndex(p => 
+            p.id === product.id &&
+            p.selectedSize === product.selectedSize && 
+            p.selectedColor === product.selectedColor
+          )
+          if (existingIndex !== -1) {
+            const updatedCart = [...state.cart]
+            updatedCart[existingIndex].quantity += product.quantity || 1
+            return {cart: updatedCart};
+          }
+          return {
+            cart: [
+              ...state.cart,
+              {
+                ...product,
+                quantity: product.quantity || 1,
+                selectedSize: product.selectedSize,
+                selectedColor: product.selectedColor,
+              }
+            ]
+          }
+        }),
+
+      removeFromCart: (product) =>
+        set((state) => ({
+          cart: state.cart.filter((p) => p.id !== product.id),
+          product,
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
 );
 
 export default useCartStore;
