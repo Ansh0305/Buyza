2f6a083cb5925fce8d48b1d5e8e1834924c81cf8
Ansh0305 <saiansh2016@gmail.com>
2026-01-24 00:27:15 +0530

    Increment and Decrement of quantity using buttons is implemented

--- client/src/components/ProductInteraction.tsx
+++ client/src/components/ProductInteraction.tsx
@@ -25,6 +25,14 @@ const ProductInteraction = ({
       params.set(type, value)
       router.push(`${pathname}?${params.toString()}`, { scroll: false });
   }
+
+  const handleQuantityChange =(type: "increment" | "decrement") => {
+    if (type === "increment") {
+      setQuantity((prev) => prev + 1)
+    } else if (quantity > 1) {
+        setQuantity((prev) => prev - 1)
+    }
+  }
   return (
     <div className="flex flex-col gap-4 mt-4">
       {/* Size */}
@@ -77,15 +85,20 @@ const ProductInteraction = ({
       <div className="flex flex-col gap-2 text-sm">
         <span className="text-gray-500">Quantity</span>
         <div className="flex items-center gap-2">
-
-          <button className="cursor-pointer border-1 border-gray-300 p-1">
-            <Minus className="w-4 h-4"/>
+          <button
+            className="cursor-pointer border-1 border-gray-300 p-1"
+            onClick={() => handleQuantityChange("decrement")}
+          >
+            <Minus className="w-4 h-4" />
           </button>
 
           <span>{quantity}</span>
 
-          <button className="cursor-pointer border-1 border-gray-300 p-1">
-            <Plus className="w-4 h-4"/>
+          <button
+            className="cursor-pointer border-1 border-gray-300 p-1"
+            onClick={() => handleQuantityChange("increment")}
+          >
+            <Plus className="w-4 h-4" />
           </button>
         </div>
       </div>
