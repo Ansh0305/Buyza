ae4a438645e8d3410a4493bd16f7743bc5dda856
Ansh0305 <saiansh2016@gmail.com>
2026-01-23 22:05:31 +0530

    Issues with shopping cart icon regarding hydration. Need to be fixed

--- client/src/components/ShoppingCartIcon.tsx
+++ client/src/components/ShoppingCartIcon.tsx
@@ -1,22 +1,24 @@
-"use client"
+"use client";
 
 import useCartStore from "@/app/stores/cartStore";
 import { ShoppingCart } from "lucide-react";
-import Link from "next/link"
+import Link from "next/link";
 
 const ShoppingCartIcon = () => {
+  const { cart, hasHydrated } = useCartStore();
 
-  const {cart} = useCartStore();
+  // if (!hasHydrated) return null;
+
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
-    <div>
-      <Link href="/cart" className="relative">
-        <ShoppingCart className="w-4 h-4 text-gray-600" />
-        <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
-          {cart.reduce((acc, item) => acc + item.quantity, 0)}
-          </span>
-      </Link>
-    </div>
+    <Link href="/cart" className="relative">
+      <ShoppingCart className="w-4 h-4 text-gray-600" />
+      <span className="absolute -top-3 -right-3 bg-amber-400 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
+        {totalItems}
+      </span>
+    </Link>
   );
-}
+};
 
 export default ShoppingCartIcon;
