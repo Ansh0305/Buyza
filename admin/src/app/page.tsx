275537c56bbfeeec1b1f5687e8765eb70a4b60de
Ansh0305 <saiansh2016@gmail.com>
2026-01-24 14:18:23 +0530

    ShadCN Variants and Custom Styling

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,9 +1,62 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {MousePointerClick} from "lucide-react"
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="">
-      
+    <div className="h-screen flex items-center justify-center">
+      <Button
+        variant="default"
+        size="xs"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
+
+      <Button
+        variant="destructive"
+        size="sm"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="lg"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
+
+      <Button
+        variant="link"
+        size="xl"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
+
+      <Button
+        variant="outline"
+        size="xxl"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
+
+      <Button
+        variant="secondary"
+        size="xxxl"
+        className="rounded-full cursor-pointer"
+      >
+        <MousePointerClick />
+        Click me
+      </Button>
     </div>
   );
 }
