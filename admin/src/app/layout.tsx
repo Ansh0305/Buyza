e8e6c266adc37751448498c1d5f7f711e7c670a4
Ansh0305 <saiansh2016@gmail.com>
2026-01-29 16:46:29 +0530

    Added Navbar and sidebar(permanent)

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
+import AppSidebar from "@/components/AppSidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        {children}
+        <AppSidebar />
+        <main className="w-full">
+          <NavBar />
+          <div className="px-4">{children}</div>
+        </main>
       </body>
     </html>
   );
