f1938551c448b549070665a5b5c0cbda9c20000f
Ansh0305 <saiansh2016@gmail.com>
2026-01-30 16:00:51 +0530

    Sidebar Structure and adding icons from lucide

--- admin/components/AppSidebar.tsx
+++ admin/components/AppSidebar.tsx
@@ -1,7 +1,90 @@
+import { Home, Inbox, Calendar, Search, Settings, User2, ChevronUp } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar"
+import Link from "next/link";
+import Image from "next/image";
+import { DropdownMenu } from "./ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
 const AppSidebar = () => {
   return (
-    <div className=''>AppSidebar</div>
-  )
+    <Sidebar className="">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image src="/logo.png" alt="logo" width={20} height={20} />
+                <span>Buyza</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+           <SidebarMenuItem>
+             <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User2/> UNKNOWN <ChevronUp className="ml-auto"/>
+                  </SidebarMenuButton>
+              </DropdownMenuTrigger>
+             </DropdownMenu>
+           </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
 
 export default AppSidebar
