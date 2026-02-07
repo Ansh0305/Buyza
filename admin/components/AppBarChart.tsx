8d2be1b292dfbbba0d39968b5b000c3c908fd5e0
Ansh0305 <saiansh2016@gmail.com>
2026-02-07 15:53:00 +0530

    Created Basic Barchart using shadcn

--- admin/components/AppBarChart.tsx
+++ admin/components/AppBarChart.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
-import { ChartContainer, type ChartConfig } from "@/components/ui/chart";
-import { Bar, BarChart, CartesianGrid } from "recharts";
+import { ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent, type ChartConfig } from "@/components/ui/chart";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "#2563eb",
+    color: "var(--chart-1)",
   },
   mobile: {
     label: "Mobile",
-    color: "#60a5fa",
+    color: "var(--chart-4)",
   },
 } satisfies ChartConfig;
 
@@ -21,21 +21,31 @@ const chartData = [
   { month: "April", desktop: 73, mobile: 190 },
   { month: "May", desktop: 209, mobile: 130 },
   { month: "June", desktop: 214, mobile: 140 },
-];  
+];
 
 const AppBarChart = () => {
   return (
     <div className="">
-      <h1>Total Revenue</h1>
+      <h1 className="text-lg font-medium mb-6">Total Revenue</h1>
       <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
         <BarChart accessibilityLayer data={chartData}>
-          <CartesianGrid vertical={false} />
+          <CartesianGrid vertical={true} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
           <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
           <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
         </BarChart>
       </ChartContainer>
     </div>
   );
-}
+};
 
-export default AppBarChart
+export default AppBarChart;
