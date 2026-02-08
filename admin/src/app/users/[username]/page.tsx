b4bafb4632108d7f086530bb947922b4823ac9ea
Ansh0305 <saiansh2016@gmail.com>
2026-02-08 13:00:49 +0530

    Single user page

--- admin/src/app/users/[username]/page.tsx
+++ admin/src/app/users/[username]/page.tsx
@@ -33,7 +33,9 @@ const SingleUserPage = () => {
           {/* User Badges container */}
           <div className="bg-primary-foreground p-4 rounded-lg">Badges</div>
           {/* User Information container */}
-          <div className="bg-primary-foreground p-4 rounded-lg">Information</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            Information
+          </div>
           {/* card List container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
             <CardList title="Recent Transactions" />
@@ -41,9 +43,9 @@ const SingleUserPage = () => {
         </div>
         {/* RIGHT */}
         <div className="w-full xl:w-2/3 space-y-6">
-        {/* User Card container */}
+          {/* User Card container */}
           <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
-        {/* Chart container */}
+          {/* Chart container */}
           <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
         </div>
       </div>
