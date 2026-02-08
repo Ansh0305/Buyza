343a6d6249febc256ec26431d74045ed67094fd1
Ansh0305 <saiansh2016@gmail.com>
2026-02-08 12:59:57 +0530

    Creating Users page

---
+++ admin/src/app/users/[username]/page.tsx
@@ -0,0 +1,54 @@
+import CardList from "@/components/CardList";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+const SingleUserPage = () => {
+  return (
+    <div className="">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Jonny</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* LEFT */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* User Badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Badges</div>
+          {/* User Information container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Information</div>
+          {/* card List container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full xl:w-2/3 space-y-6">
+        {/* User Card container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+        {/* Chart container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
