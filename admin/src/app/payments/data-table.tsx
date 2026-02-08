9ac3a1763ac295c257219cbddf7e7968807d3eaa
Ansh0305 <saiansh2016@gmail.com>
2026-02-08 18:39:06 +0530

    Imported DataTablePagination component

--- admin/src/app/payments/data-table.tsx
+++ admin/src/app/payments/data-table.tsx
@@ -17,6 +17,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { DataTablePagination } from "@/components/DataTablePagination";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -79,7 +80,7 @@ export function DataTable<TData, TValue>({
         </TableBody>
       </Table>
 
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
@@ -96,7 +97,10 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div>
+      </div> */}
+
+        <DataTablePagination table={table}/>
+
     </div>
   );
 }
