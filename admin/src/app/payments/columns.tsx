a74c9ad9902b91cf58ac03e55458b5e0fe16fb67
Ansh0305 <saiansh2016@gmail.com>
2026-02-08 18:24:03 +0530

    Tanstack Table custon headers and cells

--- admin/src/app/payments/columns.tsx
+++ admin/src/app/payments/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 
 export type Payment = {
@@ -10,22 +11,46 @@ export type Payment = {
   status: "pending" | "processing" | "success" | "failed";
 };
 
-
 export const columns: ColumnDef<Payment>[] = [
-    {
-        accessorKey: "username",
-        header: "User",
+  {
+    accessorKey: "username",
+    header: "User",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+
+      return (
+        <div
+          className={cn(
+            `p-1 rounded-md w-max text-xs`,
+            status === "pending" && "bg-yellow-500/40",
+            status === "success" && "bg-green-500/40",
+            status === "failed" && "bg-red-500/40",
+          )}
+        >
+          {status as string}
+        </div>
+      );
     },
-    {
-        accessorKey: "email",
-        header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "INR",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
     },
-    {
-      accessorKey: "status",
-      header: "Status",
-    },
-    {
-        accessorKey: "amount",
-        header: "Amount",
-    },
+  },
 ];
