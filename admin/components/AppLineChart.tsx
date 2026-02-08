b421cd6d342d02842c325f0ee37c518d23ad3f02
Ansh0305 <saiansh2016@gmail.com>
2026-02-08 17:42:31 +0530

    Added Line Chart using shadcn

--- admin/components/AppLineChart.tsx
+++ admin/components/AppLineChart.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { ChartConfig } from "./ui/chart";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart";
 
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
@@ -24,8 +25,46 @@ const chartConfig = {
 
 const AppLineChart = () => {
   return (
-    <div className=''>AppLineChart</div>
-  )
+    <ChartContainer config={chartConfig} className="mt-6">
+      <LineChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <YAxis
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+        />
+        <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+        <Line
+          dataKey="desktop"
+          type="monotone"
+          stroke="var(--color-desktop)"
+          strokeWidth={2}
+          dot={false}
+        />
+        <Line
+          dataKey="mobile"
+          type="monotone"
+          stroke="var(--color-mobile)"
+          strokeWidth={2}
+          dot={false}
+        />
+      </LineChart>
+    </ChartContainer>
+  );
 }
 
 export default AppLineChart
--- admin/src/app/users/[username]/page.tsx
+++ admin/src/app/users/[username]/page.tsx
@@ -179,6 +179,7 @@ const SingleUserPage = () => {
           </div>
           {/* Chart container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Activity</h1>
             <AppLineChart/>
           </div>
         </div>
