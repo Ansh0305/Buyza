b59a44d668d8aedf53ddb5d0096b2a1cdeb05c73
Ansh0305 <saiansh2016@gmail.com>
2026-07-06 17:48:52 +0530

    Initialized index.ts

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,1 +1,15 @@
 import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+}))
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
+})
