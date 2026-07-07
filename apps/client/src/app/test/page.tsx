1b812ef9fe570064acc66711accbf2119476c9f6
Ansh0305 <saiansh2016@gmail.com>
2026-07-07 19:54:58 +0530

    add authenticated request to Hono payment service

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -1,27 +1,36 @@
 import { auth } from "@clerk/nextjs/server";
 
 const TestPage = async () => {
-    
-    const {getToken} = await auth();
-    const token = await getToken();
 
-    // Express Microservice(Product)
-    const resProduct = await fetch("http://localhost:8000/test", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const dataProduct = await resProduct.json();
-    console.log(dataProduct);
+  const { getToken } = await auth();
+  const token = await getToken();
 
-    // Fastify Microservice(Order)
-    const resOrder = await fetch("http://localhost:8001/test", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const dataOrder = await resOrder.json();
-    console.log(dataOrder);
+  // Express Microservice(Product)
+  const resProduct = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataProduct = await resProduct.json();
+  console.log(dataProduct);
+
+  // Fastify Microservice(Order)
+  const resOrder = await fetch("http://localhost:8001/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataOrder = await resOrder.json();
+  console.log(dataOrder);
+
+  // Hono Microservice(Payment)
+  const resPayment = await fetch("http://localhost:8002/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataPayment = await resPayment.json();
+  console.log(dataPayment);
 
   return (
     <div>TestPage</div>
