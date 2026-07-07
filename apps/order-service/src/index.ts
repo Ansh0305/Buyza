62bdff7d0d793308d86136d564c9102aeeea8bf0
Ansh0305 <saiansh2016@gmail.com>
2026-07-07 16:48:33 +0530

    Testing api's(HONO, FASTIFY, EXPRESS)

--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -2,6 +2,14 @@ import Fastify from "fastify";
 
 const fastify = Fastify();  
 
+fastify.get("/health", (request, reply) => {
+  return reply.status(200).send({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
 const start = async () => {
     const port = Number(process.env.PORT) || 8001;
     try {
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 
 const app = express();
@@ -8,8 +8,16 @@ app.use(cors({
     credentials: true,
 }))
 
-const port = process.env.PORT;
+app.get("/health", (req: Request, res: Response) =>{
+    return res.status(200).json({
+      status: "ok",
+      uptime: process.uptime(),
+      timestamp: Date.now(),
+    });
+})
 
+
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
 })
