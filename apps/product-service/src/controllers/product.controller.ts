b189e0d19b5c69f9052fe85f344bbf741e224a74
Ansh0305 <saiansh2016@gmail.com>
2026-07-17 21:30:07 +0530

    validate colors and images before product creation

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -4,25 +4,40 @@ import { prisma, Prisma } from "@repo/product-db";
 export const createProduct = async (req: Request, res: Response) => {
     const data: Prisma.ProductCreateInput = req.body;
 
-    const product = await prisma.product.create({data});
+    const { colors, images } = data;
+    if (!colors || !Array.isArray(colors) || colors.length == 0){
+        return res.status(400).json({message: "Colors array is required!"})
+    }
+
+    if (!images || typeof images !== "object") {
+        return res.status(400).json({ message: "Images object is required!" });
+    }
+
+    const missingColors = colors.filter((color) => !(color in images));
+
+    if (missingColors.length > 0) {
+        return res.status(400).json({ message: "Missing images for colors!", missingColors });
+    }
+
+      const product = await prisma.product.create({ data });
     res.status(201).json(product)
 
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
-    
+
 }
 
 export const deleteProduct = async (req: Request, res: Response) => {
-    
+
 }
 
 // All Products
 export const getProducts = async (req: Request, res: Response) => {
-    
+
 }
 
 // Single product
 export const getProduct = async (req: Request, res: Response) => {
-    
+
 }
