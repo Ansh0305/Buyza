4225a75c06a9e956a8bf3d3219a80ea1c77df3bb
Ansh0305 <saiansh2016@gmail.com>
2026-07-17 16:32:19 +0530

    Imported routes and also handled errors

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,34 +1,48 @@
-import express, {Request, Response} from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { clerkMiddleware, getAuth } from "@clerk/express";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
+import productRouter from "./routes/product.route.js";
+import categoryRouter from "./routes/category.route.js";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
-}))
+  }),
+);
+
+app.use(express.json);
 app.use(clerkMiddleware());
 
-app.get("/health", (req: Request, res: Response) =>{
-    return res.status(200).json({
-      status: "ok",
-      uptime: process.uptime(),
-      timestamp: Date.now(),
-    });
-})
+app.get("/health", (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
 
-app.get("/test", shouldBeUser,  (req:Request, res:Response) => {
+app.get("/test", shouldBeUser, (req: Request, res: Response) => {
+  res.json({
+    message: "Product Servive auth!",
+    userId: req.userId,
+  });
+});
 
-    res.json({
-        message: "Product Servive auth!", 
-        userId: req.userId
-    })
-})
+app.use("/products", productRouter);
+app.use("/categories", categoryRouter);
 
+app.use(
+  async (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    return res.status(err.status || 500).json({message: err.message || "Internal Server Error!"})
+  },
+);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}!`)
-})
+  console.log(`Listening on port ${port}!`);
+});
