9bd027108145f20ff04f388b956d349b338bba53
Ansh0305 <saiansh2016@gmail.com>
2026-07-17 21:54:19 +0530

    add sorting, filtering, and search to product listing

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -2,24 +2,32 @@ import { Request, Response } from "express"
 import { prisma, Prisma } from "@repo/product-db";
 
 export const createProduct = async (req: Request, res: Response) => {
-    const data: Prisma.ProductCreateInput = req.body;
+    const { categorySlug, colors, images, ...rest } = req.body;
 
-    const { colors, images } = data;
-    if (!colors || !Array.isArray(colors) || colors.length == 0){
-        return res.status(400).json({message: "Colors array is required!"})
+    if (!colors || !Array.isArray(colors) || colors.length == 0) {
+        return res.status(400).json({ message: "Colors array is required!" })
     }
 
     if (!images || typeof images !== "object") {
         return res.status(400).json({ message: "Images object is required!" });
     }
 
-    const missingColors = colors.filter((color) => !(color in images));
+    const missingColors = colors.filter((color: string) => !(color in images));
 
     if (missingColors.length > 0) {
         return res.status(400).json({ message: "Missing images for colors!", missingColors });
     }
 
-      const product = await prisma.product.create({ data });
+    const product = await prisma.product.create({
+        data: {
+            ...rest,
+            colors,
+            images,
+            category: {
+                connect: { slug: categorySlug },
+            },
+        },
+    });
     res.status(201).json(product)
 
 }
@@ -35,6 +43,38 @@ export const deleteProduct = async (req: Request, res: Response) => {
 // All Products
 export const getProducts = async (req: Request, res: Response) => {
 
+    const { sort, category, search, limit } = req.query;
+    const orderBy = (() => {
+        switch (sort) {
+            case "asc":
+                return { price: Prisma.SortOrder.asc };
+                break;
+            case "desc":
+                return { price: Prisma.SortOrder.desc };
+                break;
+            case "oldest":
+                return { createdAt: Prisma.SortOrder.asc };
+                break;
+            default:
+                return { createdAt: Prisma.SortOrder.desc };
+                break;
+        }
+    })();
+
+    const products = await prisma.product.findMany({
+        where: {
+            category: {
+                slug: category as string,
+            },
+            name:{
+                contains: search as string,
+                mode: "insensitive"
+            }
+        },
+        orderBy,
+        take: limit ? Number(limit) : undefined,
+    });
+    res.status(200).json(products); 
 }
 
 // Single product
